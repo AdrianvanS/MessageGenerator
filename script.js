4f52284990d7dca5a9be70af73bea3d5436e8d00
Adrian_vSk <agvsmail@gmail.com>
2021-05-30 16:55:51 +0100

    Created 3 arrays to store components of message.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const names = ["Bob", "Phil", "James", "Kim", "Noel", "William", "John", "Sharon", "Kelly", "Larry", "Susie", "Margaret", "Sally", "Esther"];
+
+const compliment = ["awesome", "lovely", "brilliant", "excellent", "amazing", "wonderful", "the best", "fantastic", "superb"];
+
+const message = ["have a great day", "have an excellent day", "are having the best day", "get the best out of today", "are really happy", "get plenty of fresh air", "realize how special you are", "get a nice surprise today"];
+
 function newMessage(){
-    document.getElementById("message").innerHTML = "This is a new message.";
+    const nameNum = Math.floor(Math.random() * names.length);
+    const complimentNum = Math.floor(Math.random() * compliment.length);
+    const messageNum = Math.floor(Math.random() * message.length);
+
+    document.getElementById("message").innerHTML = `${names[nameNum]} said: You are ${compliment[complimentNum]}. I hope you ${message[messageNum]}.`;
 };
